2285254a090278025d33205884cbba8568f84ec0
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2022-06-23 14:26:59 +0300

    feat(10526): done

--- src/common/formatImageUrl.ts
+++ src/common/formatImageUrl.ts
@@ -1,5 +1,5 @@
 const formatImageUrl = (url: string | null | undefined) => {
-  const baseCdnUrl = 'https://rollun-cdn.sfo2.digitaloceanspaces.com';
+  const baseCdnUrl = 'https://rollun.s3.eu-central-1.amazonaws.com';
   return url === '-1' || !url ? `${baseCdnUrl}/images/no-image.png` : url;
 };
 
